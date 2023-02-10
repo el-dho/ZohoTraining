05cd297885dc804a103d16e9821fcffba5d3e149
eldho-pt7166 <eldho-pt7166@192.168.18.123>
2023-02-10 15:38:32 +0530

    Aggregation

---
+++ Training2/src/inheritance/Aggregation.java
@@ -0,0 +1,36 @@
+package inheritance;
+
+class Team{
+	String team;
+	Team(String team){
+		this.team = team;
+	}
+}
+
+class EmployeeZ{
+	String name;
+	Team team;
+	EmployeeZ(String name,Team team){
+		this.name = name;
+		this.team = team;
+	}
+	void printMethod() {
+		System.out.println("Employee Details: " + name + " " + team.team);
+	}
+}
+
+public class Aggregation {
+	public static void main(String[] args) {
+		Team t1 = new Team("Desk");
+		Team t2 = new Team("CRM");
+		
+		EmployeeZ emp1 = new EmployeeZ("Ramesh",t1);
+		EmployeeZ emp2 = new EmployeeZ("Suresh",t2);
+		EmployeeZ emp3 = new EmployeeZ("Bromesh",t1);
+		
+		emp1.printMethod();
+		emp2.printMethod();
+		emp3.printMethod();
+	}
+}
+
