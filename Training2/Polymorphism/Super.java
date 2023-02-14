2d30909ca41c593c96a35c93d8f600a1e4cce45c
eldho-pt7166 <eldho-pt7166@192.168.18.123>
2023-02-14 22:50:13 +0530

    Super Keyword

---
+++ Training2/Polymorphism/Super.java
@@ -0,0 +1,33 @@
+
+public class Super {
+	public static void main(String[] args) {
+		EmployeeComp emp = new EmployeeComp();
+		emp.variableMethod();
+		emp.superMethod();
+	}
+}
+
+
+class ZohoComp{
+	String name = "Zoho";
+	ZohoComp(){
+		System.out.println("This is inside the parent constructor using super keyword");
+	}
+	void method(String str) {
+		System.out.println("This is inside parent method and output is:" + str);
+	}
+}
+
+class EmployeeComp extends ZohoComp{
+	String name  = "EmployeeComp";
+	void variableMethod() {
+		System.out.println("This is inside the method of child class");
+		System.out.println("Variable value in parent class: " + super.name);
+	}
+	void superMethod() {
+		super.method("This is the super method");
+	}
+	EmployeeComp(){
+		super();
+	}
+}
