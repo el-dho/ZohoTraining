06e5f45036d182eac05a137e5183f49f4a972c12
eldho-pt7166 <eldho-pt7166@192.168.18.123>
2023-02-15 13:43:11 +0530

    Interface

---
+++ Training2/Abstraction/Interface.java
@@ -0,0 +1,26 @@
+
+public class Interface {
+	public static void main(String[] args) {
+		CompanyXYZ comp;
+		comp = new Nike();
+		comp.getName();
+		comp = new Puma();
+		comp.getName();
+	}
+}
+
+interface CompanyXYZ{
+	void getName();
+	}
+
+class Nike implements CompanyXYZ{
+	public void getName() {
+		System.out.println("This class is Nike");
+	}
+}
+
+class Puma implements CompanyXYZ{
+	public void getName() {
+		System.out.println("This class is Puma");
+	}
+}
