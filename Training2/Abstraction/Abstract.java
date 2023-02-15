8682fe2006006bb5acd8e51c6a1d693abe2bd0bf
eldho-pt7166 <eldho-pt7166@192.168.18.123>
2023-02-15 13:25:54 +0530

    Abstract for templates

---
+++ Training2/Abstraction/Abstract.java
@@ -0,0 +1,25 @@
+
+public class Abstract {
+	public static void main(String[] args) {
+		Comp obj = new CompA();
+		obj.printMethod();
+		obj = new CompB();
+		obj.printMethod();
+	}
+}
+
+abstract class Comp{
+	abstract void printMethod();
+}
+
+class CompA extends Comp{
+	void printMethod() {
+		System.out.println("This is Company A");
+	}
+}
+
+class CompB extends Comp{
+	void printMethod() {
+		System.out.println("This is Company B");
+	}
+}
