746dea040a2bee96d67abcddb013c91193f1238b
el-dho <66713416+el-dho@users.noreply.github.com>
2023-02-22 11:10:55 +0530

    Ex3 added

--- Exercises/Ex3/src/Main.java
+++ Exercises/Ex3/src/Main.java
@@ -1,6 +1,34 @@
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Browser obj = new Browser();
+		obj.setTabs(5);
+		Integer tabMain = obj.getTabs(); //auto boxing
+		System.out.println("After Auto Boxing : " + tabMain);
+		obj.setTabInt(9);
+		int tabIntMain = obj.getTabInt(); //unboxing
+		System.out.println("After unboxing : " + tabIntMain);
 	}
+}
+
+class Browser {
+	private int tabs;
+	private Integer tabInt;
+
+	public Integer getTabInt() {
+		return tabInt;
+	}
+
+	public void setTabInt(Integer tabInt) {
+		this.tabInt = tabInt;
+	}
+
+	public int getTabs() {
+		return tabs;
+	}
+
+	public void setTabs(int tabs) {
+		this.tabs = tabs;
+	}
+	
 }
